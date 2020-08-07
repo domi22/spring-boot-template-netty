ab1a53adb470d0178a68cea19a4d1ad297411f65
domi22 <491431567@qq.com>
2020-08-07 23:55:48 +0800

    netty-server

--- src/main/java/spring/boot/template/servers/Server.java
+++ src/main/java/spring/boot/template/servers/Server.java
@@ -5,10 +5,12 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioChannelOption;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
 import spring.boot.template.server.codec.OrderFrameDecoder;
 import spring.boot.template.server.codec.OrderFrameEncoder;
 import spring.boot.template.server.codec.OrderprotocolDecoder;
@@ -22,9 +24,18 @@ public class Server {
 
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.channel(NioServerSocketChannel.class);
-        serverBootstrap.group(new NioEventLoopGroup());
+
+        //完善线程的名字
+        NioEventLoopGroup group = new NioEventLoopGroup(0,new DefaultThreadFactory("dome"));
+        NioEventLoopGroup worker = new NioEventLoopGroup(0,new DefaultThreadFactory("dome-worker"));
+
+        serverBootstrap.group(group,worker);
         //日志打印
         serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+
+        //小文件报文的时候提高效率
+        serverBootstrap.childOption(NioChannelOption.TCP_NODELAY, true);
+        serverBootstrap.option(NioChannelOption.SO_BACKLOG, 1024);
 
         serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
@@ -37,7 +48,7 @@ public class Server {
                 pipeline.addLast(new OrderprotocolDecoder());
                 //日志打印
                 pipeline.addLast(new LoggingHandler(LogLevel.INFO));
-                pipeline.addLast(new OrderServerProcessHandler());
+                pipeline.addLast("orderHandler",new OrderServerProcessHandler());
             }
         });
 
